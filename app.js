a981b9fbfd8596529fd58cf8643e23f845909144
Miranda Bånnsgård <bannsgard3@gmail.com>
2023-09-18 18:58:34 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'green';
+ctx.fillRect(0, 10, 10, 100);
+ctx.fillRect(20, 10, 5, 100);
+ctx.fillRect(30, 10, 5, 100);
