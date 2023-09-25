467268cfc7020619bb4e1811dc0381170f27c371
Miranda Bånnsgård <bannsgard3@gmail.com>
2023-09-25 13:06:33 +0200

    Add method drawLine

--- BarChart.js
+++ BarChart.js
@@ -20,6 +20,11 @@ export default class BarChart {
     this.#drawDot(5, 295, 5);
     this.#drawDot(595, 295, 5);
     this.#drawDot(595, 5, 5);
+
+    this.#drawLine(5,5,595, 5, 10);
+    this.#drawLine(5,5,5,295, 10);
+    this.#drawLine(5,295,595,295, 10);
+    this.#drawLine(595,5,595, 295, 10);
   }
 
   #drawDot(x, y, size) {
@@ -27,6 +32,15 @@ export default class BarChart {
     this.#context.arc(x, y, size, 0, 2 * Math.PI);
     this.#context.fillStyle = "#346823";
     this.#context.fill();
+  }
+
+  #drawLine(startX, startY, endX, endY, lineWidth) {
+    this.#context.beginPath();
+    this.#context.moveTo(startX, startY);
+    this.#context.lineTo(endX, endY);
+    this.#context.strokeStyle = "#346823";
+    this.#context.lineWidth = lineWidth
+    this.#context.stroke();
   }
 
 }
