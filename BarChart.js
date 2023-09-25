53a4a7e48deaf590a353bd6276069b04699b7e09
Miranda Bånnsgård <bannsgard3@gmail.com>
2023-09-25 11:07:15 +0200

    Double the size of BarChart canvas

--- BarChart.js
+++ BarChart.js
@@ -6,14 +6,20 @@ export default class BarChart {
     this.#canvas = canvas;
     this.#context = canvas.getContext('2d');
 
-    console.log(this.#canvas.width) // 300
-    console.log(this.#canvas.height) // 150
+    console.log(this.#canvas.width); // default: 300
+    console.log(this.#canvas.height); // default: 150
 
-    this.#drawDot(5,5, 5);
-    this.#drawDot(5,100, 5);
-    this.#drawDot(100,100, 5);
-    this.#drawDot(100,5, 5);
+    // default width of canvas is 300, change it 600
+    this.#canvas.width = 600;
 
+    // default height of canvas is 150, change it to 300
+    this.#canvas.height = 300;
+
+    // draw a test dot in each corner of canvas
+    this.#drawDot(5, 5, 5);
+    this.#drawDot(5, 295, 5);
+    this.#drawDot(595, 295, 5);
+    this.#drawDot(595, 5, 5);
   }
 
   #drawDot(x, y, size) {
