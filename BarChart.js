e5fe02c622a9d545f60e708ccadf834721d9eac0
Miranda Bånnsgård <bannsgard3@gmail.com>
2023-09-25 14:42:44 +0200

    Add drawRectangle method with a randomColor method

--- BarChart.js
+++ BarChart.js
@@ -21,10 +21,22 @@ export default class BarChart {
     this.#drawDot(595, 295, 5);
     this.#drawDot(595, 5, 5);
 
+    // draw test line connecting the dots
     this.#drawLine(5,5,595, 5, 10);
     this.#drawLine(5,5,5,295, 10);
     this.#drawLine(5,295,595,295, 10);
     this.#drawLine(595,5,595, 295, 10);
+
+    // draw y axis
+    this.#drawLine(50,50,50, this.#canvas.height-30, 2);
+
+    //draw x axis
+    this.#drawLine(50,this.#canvas.height-30,this.#canvas.width-40, this.#canvas.height-30, 2);
+
+    this.#drawRectangle(100, this.#canvas.height-100, 50, 70);
+    this.#drawRectangle(200, this.#canvas.height-120, 50, 90);
+    this.#drawRectangle(300, this.#canvas.height-110, 50, 80);
+    this.#drawRectangle(400, this.#canvas.height-200, 50, 170);
   }
 
   #drawDot(x, y, size) {
@@ -39,8 +51,23 @@ export default class BarChart {
     this.#context.moveTo(startX, startY);
     this.#context.lineTo(endX, endY);
     this.#context.strokeStyle = "#346823";
-    this.#context.lineWidth = lineWidth
+    this.#context.lineWidth = lineWidth;
     this.#context.stroke();
+  }
+
+  #drawRectangle (posX, posY, width, height) {
+    this.#context.fillStyle = this.#getRandomColor();
+    this.#context.fillRect(posX, posY, width, height);
+  }
+
+  #getRandomColor () {
+    const hexDigits = '0123456789ABCDEF';
+    let color = '#';
+
+    for (let i = 0; i < 6; i++) {
+      color += hexDigits[Math.floor(Math.random() * 16)];
+    }
+    return color;
   }
 
 }
