1c35ef0423746711827a89da84db11578180f2e3
Miranda Bånnsgård <bannsgard3@gmail.com>
2023-09-25 08:52:27 +0200

    Create a BarChart to test the module BarChart

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+import BarChart from './BarChart.js'
 
-ctx.fillStyle = 'green';
-ctx.fillRect(0, 10, 10, 100);
-ctx.fillRect(20, 10, 5, 100);
-ctx.fillRect(30, 10, 5, 100);
+const canvas = document.getElementById('canvas');
+const barChart = new BarChart(canvas);
+// barChart.drawGrid()
