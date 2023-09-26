743cfd1562e8bc297ce5d56bb39b10b16c90cf53
Miranda Bånnsgård <bannsgard3@gmail.com>
2023-09-26 08:25:23 +0200

    Update method with id as argument

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import BarChart from './BarChart.js'
 
-const canvas = document.getElementById('canvas');
-const barChart = new BarChart(canvas);
+
+const canvas2 = document.getElementById('canvas2');
+const barChart1 = new BarChart('canvas1');
+const barChart2 = new BarChart('canvas2');
 // barChart.drawGrid()
