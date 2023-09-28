b5f075f1be7ed6dac63c1c39d35947ffdee25880
Miranda Bånnsgård <bannsgard3@gmail.com>
2023-09-28 09:51:49 +0200

    Update test app with setData

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ import BarChart from './BarChart.js'
 
 const barchart = new BarChart('canvas');
 
+const title = 'Favorite season'
 const labels = ['Spring', 'Summer', 'Autumn', 'Winter', 'None']
+const data = [10, 30, 6, 2, 10]
 
 barchart.setHeight(450)
-barchart.setWidth(600)
+barchart.setWidth(800)
+barchart.setTitle(title)
 barchart.drawXLabels(labels)
+barchart.setData(data)
+
 // const barchart2 = new BarChart('canvas2');
 //barChart.drawGrid()
