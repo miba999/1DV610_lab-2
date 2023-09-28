d1f459db254173821fb645123e17739d5ba7b2f2
Miranda Bånnsgård <bannsgard3@gmail.com>
2023-09-28 16:36:38 +0200

    Add some more data points to second chart

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const xLabel = 'Seasons'
 const yLabel = 'Number of votes'
 const categories = ['Spring', 'Summer', 'Autumn', 'Winter']
 const data = [11, 28, 4, 13]
-barchart.setHeight(600)
-barchart.setWidth(800)
+barchart.setHeight(400)
+barchart.setWidth(500)
 barchart.setTitle(title)
 barchart.setXAxisLabel(xLabel)
 barchart.setYAxisLabel(yLabel)
@@ -18,10 +18,10 @@ const barchart2 = new BarChart('canvas2');
 const title2 = 'Favorite color'
 const xLabel2 = 'Colors'
 const yLabel2 = 'Number of votes'
-const categories2 = ['Red', 'Green', 'Blue', 'Yellow', 'White', 'Black']
-const data2 = [11, 28, 4, 113, 3, 10]
+const categories2 = ['Red', 'Green', 'Blue', 'Yellow', 'White', 'Black', 'Orange', 'Purple', 'Pink', 'Brown', 'Gray', 'Other']
+const data2 = [11, 28, 4, 50, 3, 100, 23, 52, 73, 23, 21, 34]
 barchart2.setHeight(400)
-barchart2.setWidth(400)
+barchart2.setWidth(800)
 barchart2.setTitle(title2)
 barchart2.setXAxisLabel(xLabel2)
 barchart2.setYAxisLabel(yLabel2)
