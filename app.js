6886dde1ce4a3e99cf74cc2e4c4de9d8f6866bba
Miranda Bånnsgård <bannsgard3@gmail.com>
2023-09-28 07:34:52 +0200

    Update test app with setHeight and setWidth

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import BarChart from './BarChart.js'
 
 const barchart = new BarChart('canvas');
+barchart.setHeight(500)
+barchart.setWidth(1000)
 // const barchart2 = new BarChart('canvas2');
 //barChart.drawGrid()
